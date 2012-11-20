bf75bc863153c832c76ae90ea92f97884b8cd19b
Jason C. Huang <chaoju.huang@gmail.com>
2012-11-20 22:48:39 +0800

    Use content script

--- script/background.js
+++ script/background.js
@@ -1,9 +1,11 @@
 var checkNewest, inEpisodeList, initialize, isDebugging, ls, makeNotification, render, setLoop, updateBadge,
   __indexOf = Array.prototype.indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
 
+/*
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
   return render(tab);
 });
+*/
 
 ls = localStorage;
 
