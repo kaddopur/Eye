bf2e3946246111c443ccb6b3339f1cca6c774b2a
KeDouBo <kaddo.dev@gmail.com>
2012-12-04 16:04:00 +0800

    Refactor view.css

--- src/s.sh
+++ src/s.sh
@@ -1,5 +1,4 @@
 coffee -bw -c -o ../js . &
 jade -w -P -O .. . &
 #stylus -w -o ../css . &
-sass --watch popup.scss:../css/popup.css &
-
+sass --watch popup.scss:../css/popup.css viewer.scss:../css/viewer.css &
